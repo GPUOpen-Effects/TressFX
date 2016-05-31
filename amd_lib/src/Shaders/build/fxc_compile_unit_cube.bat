b2d7b70dac9d7a6a39ad27fb24b43d20e7d1b686
jstewart-amd <jason.stewart@amd.com>
2016-05-31 10:54:57 -0400

    Remove amd_lib in preparation for folder reorg

--- amd_lib/src/Shaders/build/fxc_compile_unit_cube.bat
+++
@@ -1,4 +0,0 @@
-fxc.exe /nologo /T vs_5_0 /E vsUnitCube      /O1 /Fh ..\inc\VS_UNIT_CUBE.inc       /Vn VS_UNIT_CUBE_Data       ..\AMD_UnitCube.hlsl
-fxc.exe /nologo /T ps_5_0 /E psUnitCube      /O1 /Fh ..\inc\PS_UNIT_CUBE.inc       /Vn PS_UNIT_CUBE_Data       ..\AMD_UnitCube.hlsl
-
-fxc.exe /nologo /T vs_5_0 /E vsClipSpaceCube /O1 /Fh ..\inc\VS_CLIP_SPACE_CUBE.inc /Vn VS_CLIP_SPACE_CUBE_Data ..\AMD_UnitCube.hlsl
